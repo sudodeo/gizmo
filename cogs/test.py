9083e2541aeb8686c4d77db2635b848b7a6dcf9d
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-10 21:23:47 +0100

    added purge messages command

--- cogs/test.py
+++ cogs/test.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 
 
-
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,6 +13,17 @@ class Test(commands.Cog):
     # async def welcome(self, ctx, ):
 
     #     await ctx.send(f'Welcome to my turf bozo <@772786473912631316>')
+
+    @commands.hybrid_command()
+    async def purge(self, ctx: commands.Context, amount: int) -> None:
+        """ Purge messages """
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(f'{amount} messages deleted')
+
+    @purge.error
+    async def purge_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+        """ Purge error """
+        await ctx.send(f'Error: {error}')
 
 
 async def setup(bot):
