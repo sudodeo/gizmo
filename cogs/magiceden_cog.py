1dc7377a33facb863ef21349a78d4d86219c2deb
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-13 18:00:53 +0100

    added check for app_commands.Choice name, should not be blank

--- cogs/magiceden_cog.py
+++ cogs/magiceden_cog.py
@@ -17,18 +17,17 @@ class MagicedenCog(commands.Cog, name="Magiceden"):
         current: str,
     ) -> List[app_commands.Choice[str]]:
 
-        if current != []:
-            db = self.bot.conn
-            # % is a wildcard
-            symbols = await db.fetch(f"""SELECT * FROM magiceden 
-                                        WHERE LOWER(name) LIKE '%{current.lower()}%' 
-                                        ORDER BY CASE WHEN LOWER(name) LIKE '{current.lower()}' THEN 0 
-                                                    WHEN LOWER(name) LIKE '{current.lower()}%' THEN 1
-                                                    WHEN LOWER(name) LIKE '%{current.lower()}' THEN 3
-                                                        ELSE 2
-                                                        END""")
-            if symbols:
-                return [app_commands.Choice(value=symbol['symbol'], name=symbol['name']) for symbol in symbols][:25]
+        db = self.bot.conn
+        # % is a wildcard
+        symbols = await db.fetch(f"""SELECT * FROM magiceden 
+                                    WHERE LOWER(name) LIKE '%{current.lower()}%' 
+                                    ORDER BY CASE WHEN LOWER(name) LIKE '{current.lower()}' THEN 0 
+                                                WHEN LOWER(name) LIKE '{current.lower()}%' THEN 1
+                                                WHEN LOWER(name) LIKE '%{current.lower()}' THEN 3
+                                                    ELSE 2
+                                                    END, name ASC LIMIT 30""")
+
+        return [app_commands.Choice(value=symbol['symbol'], name=symbol['name']) for symbol in symbols if symbol['name'] != ""][:25]
 
     @commands.hybrid_command()
     @app_commands.autocomplete(collection=collection_autocomplete)
@@ -89,8 +88,6 @@ class MagicedenCog(commands.Cog, name="Magiceden"):
     @me.error
     async def on_me_error(self, ctx: commands.Context, error):
         error = getattr(error, 'original', error)
-        if isinstance(error, HTTPException):
-            pass
         owner = self.bot.get_user(741308876204408854)
         await ctx.reply("An error occured. Contact grim.reaper#9626 for support", delete_after=5)
         await owner.send(f"{error}\n{ctx.kwargs}")
