c24c2c3db0ffbbb24a9cb340e0b1a6ab4fc6a495
crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-27 03:39:37 +0100

    added random user agent to request headers

--- apis/magiceden.py
+++ apis/magiceden.py
@@ -1,3 +1,4 @@
+import random
 import requests
 import timeit
 import aiohttp
@@ -11,13 +12,23 @@ class Magiceden:
         self.symbol = "◎"
         self.base = "https://magiceden.io/marketplace/"
         self.lamport = 1000000000
+        self.user_agents = [
+            'Windows 10/ Edge browser: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+            'Windows 7/ Chrome browser: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/47.0.2526.111 Safari/537.36',
+            'Mac OS X10/Safari browser: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, '
+            'like Gecko) Version/9.0.2 Safari/601.3.9',
+            'Linux PC/Firefox browser: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+            'Chrome OS/Chrome browser: Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/51.0.2704.64 Safari/537.36']
 
     async def get_collection_details(self, collection: str):
         fmt_collection = collection.strip().lower().replace(" ", "_")
         url = f'https://api-mainnet.magiceden.dev/v2/collections/{fmt_collection}'
         # print(url)
         session = aiohttp.ClientSession()
-        async with session.get(url, headers=self.headers) as res:
+        async with session.get(url, headers={'User-Agent': random.choice(self.user_agents)}) as res:
             # res = requests.get(url, headers=self.headers)
             # print(res.elapsed.total_seconds())
             res_json = await res.json()
--- apis/opensea.py
+++ apis/opensea.py
@@ -1,3 +1,4 @@
+import random
 import aiohttp
 
 
@@ -10,6 +11,16 @@ class Opensea:
         self.logo = "https://storage.googleapis.com/opensea-static/Logomark/Logomark-Blue.png"
         self.eth_symbol = "Ξ"
         self.sol_symbol = "◎"
+        self.user_agents = [
+            'Windows 10/ Edge browser: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+            'Windows 7/ Chrome browser: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/47.0.2526.111 Safari/537.36',
+            'Mac OS X10/Safari browser: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, '
+            'like Gecko) Version/9.0.2 Safari/601.3.9',
+            'Linux PC/Firefox browser: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+            'Chrome OS/Chrome browser: Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/51.0.2704.64 Safari/537.36']
 
     async def get_collection_details(self, collection: str):
         fmt_collection = collection.strip().lower().replace(" ", "_")
@@ -18,7 +29,7 @@ class Opensea:
 
         # print(res.elapsed.total_seconds())
         session = aiohttp.ClientSession()
-        async with session.get(url, headers=self.headers) as res:
+        async with session.get(url, headers={'User-Agent': random.choice(self.user_agents)}) as res:
             if res.status == 404:
                 await session.close()
                 return None, self.logo
