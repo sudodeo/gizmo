686b6492f0eabad86e07ffc99b23942194c3585c
crafter-deo <adesolaadeoluwa@gmail.com>
2022-08-27 19:12:15 +0100

    added code to send commands list after syncing

--- main.py
+++ main.py
@@ -60,7 +60,9 @@ class MyBot(commands.Bot):
                     synced = []
                 else:
                     synced = await ctx.bot.tree.sync()
-
+               
+                await self.get_user(self.owner_id).send(f"{'\n'.join(synced)}")
+                
                 await ctx.send(
                     f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
                 )
