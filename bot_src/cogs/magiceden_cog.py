deeeb49ba9486ab6dd24308b243d47a7d923bb8e
crafter-deo <adesolaadeoluwa@gmail.com>
2022-09-03 03:40:59 +0100

    style(project folder structure)

---
+++ bot_src/cogs/magiceden_cog.py
@@ -0,0 +1,98 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from apis.magiceden import Magiceden
+from datetime import datetime
+from typing import List
+from discord.errors import HTTPException
+
+
+class MagicedenCog(commands.Cog, name="Magiceden"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def collection_autocomplete(
+        self,
+        interaction: discord.Interaction,
+        current: str,
+    ) -> List[app_commands.Choice[str]]:
+
+        db = self.bot.conn
+        # % is a wildcard
+        symbols = await db.fetch(f"""SELECT * FROM magiceden 
+                                    WHERE LOWER(name) LIKE '%{current.lower()}%' 
+                                    ORDER BY CASE WHEN LOWER(name) LIKE '{current.lower()}' THEN 0 
+                                                WHEN LOWER(name) LIKE '{current.lower()}%' THEN 1
+                                                WHEN LOWER(name) LIKE '%{current.lower()}' THEN 3
+                                                    ELSE 2
+                                                    END, name ASC LIMIT 30""")
+
+        return [app_commands.Choice(value=symbol['symbol'], name=symbol['name']) for symbol in symbols if symbol['name'] != ""][:25]
+
+    @commands.hybrid_command()
+    @app_commands.describe(collection="The collection you want to search for")
+    @app_commands.autocomplete(collection=collection_autocomplete)
+    async def me(self, ctx: commands.Context, *, collection: str):
+        """Get collection details from Magiceden marketplace"""
+        async with ctx.typing():
+            magiceden = Magiceden()
+            collection_details = await magiceden.get_collection_details(collection)
+            time = datetime.now()
+
+            if type(collection_details) == dict:
+
+                name, image, collection_magiceden_url, logo, website, twitter, discord_server, stats = collection_details.values()
+
+                embed = discord.Embed(title=name, url=collection_magiceden_url,
+                                      color=discord.Colour.green(), timestamp=time)
+
+                embed.add_field(name="Floor Price",
+                                value=stats.get("floor price") or "None")
+                # embed.add_field(name="Total Supply", value=stats.get("total supply"), inline=False)
+                embed.add_field(name="Total Volume",
+                                value=stats.get("total volume") or "None")
+                embed.add_field(name="Avg Price 24hr",
+                                value=stats.get("avg price 24hr") or "None")
+                embed.add_field(name="Listed NFTs", value=stats.get(
+                    "listed count") or "None")
+                if website not in [None, ""]:
+                    embed.add_field(
+                        name="Website", value=f"[Visit]({website})")
+                if twitter not in [None, ""]:
+                    embed.add_field(
+                        name="Twitter", value=f"[@{twitter.split('/')[-1]}]({twitter})")
+                if discord_server not in [None, ""]:
+                    embed.add_field(
+                        name="Discord Server", value=f"[Join]({discord_server})")
+                embed.set_thumbnail(url=image)
+
+                embed.set_footer(
+                    text="Built by grim.reaper#9626", icon_url=logo)
+
+                await ctx.reply(embed=embed)
+
+            else:
+                # print(collection)
+
+                embed = discord.Embed(
+                    title="Collection not found", color=discord.Colour.red(), timestamp=time)
+                # embed.set_thumbnail(url=collection_details[1])
+                embed.add_field(
+                    name="Try", value="Checking collection on coralcube")
+                embed.set_image(
+                    url="https://c.tenor.com/8RRUPtXrEcgAAAAS/osita-osita-iheme.gif")
+                embed.set_footer(text="Built by grim.reaper#9626",
+                                 icon_url=collection_details[1])
+
+                await ctx.reply(embed=embed)
+
+    @me.error
+    async def on_me_error(self, ctx: commands.Context, error):
+        error = getattr(error, 'original', error)
+        owner = self.bot.get_user(741308876204408854)
+        await ctx.reply("An error occured. Contact grim.reaper#9626 for support", delete_after=5)
+        await owner.send(f"{error}\n{ctx.kwargs}")
+
+
+async def setup(bot):
+    await bot.add_cog(MagicedenCog(bot))
---
+++ bot_src/main.py
@@ -0,0 +1,110 @@
+from typing import Literal, Optional
+from discord.ext import commands, tasks
+# from settings import *
+import asyncpg
+import discord
+import logging
+from discord import app_commands
+from decouple import config
+import asyncio
+import os
+import aiohttp
+
+
+DISCORD_BOT_TOKEN = config('DISCORD_BOT_TOKEN')
+
+# handler = logging.FileHandler(filename='gizmo.log', encoding='utf-8', mode='a')
+MY_GUILD = discord.Object(id=935741302769844244)
+
+
+class Gizmo(commands.Bot):
+    POSTGRES_URI = config('POSTGRES_URI')
+
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.members = True
+        intents.message_content = True
+        super().__init__(command_prefix=commands.when_mentioned_or('>'), intents=intents)
+        # self.initial_extensions = [
+        #     'cogs.',
+        #     'cogs.foo',
+        #     'cogs.bar',
+        # ]
+        # self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        # self.background_task.start()
+        self.loop.create_task(self.startup())
+        self.session = aiohttp.ClientSession()
+        self.conn = await asyncpg.connect(self.POSTGRES_URI)
+
+        for file_name in os.listdir("./cogs"):
+
+            if file_name.endswith(".py") and file_name != "__init__.py":
+                await self.load_extension(f"cogs.{file_name[:-3]}")
+
+        @self.command()
+        @commands.is_owner()
+        @commands.guild_only()
+        async def sync(
+                ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+            if not guilds:
+                if spec == "~":
+                    synced = await ctx.bot.tree.sync(guild=MY_GUILD)
+                elif spec == "*":
+                    ctx.bot.tree.copy_global_to(guild=MY_GUILD)
+                    synced = await ctx.bot.tree.sync(guild=MY_GUILD)
+                elif spec == "^":
+                    ctx.bot.tree.clear_commands(guild=MY_GUILD)
+                    await ctx.bot.tree.sync(guild=MY_GUILD)
+                    synced = []
+                else:
+                    synced = await ctx.bot.tree.sync()
+
+                await ctx.send(
+                    f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+                )
+
+                await self.get_user(self.owner_id).send(f"{synced}")
+                return
+
+            ret = 0
+            for guild in guilds:
+                try:
+                    await ctx.bot.tree.sync(guild=guild)
+                except discord.HTTPException:
+                    pass
+                else:
+                    ret += 1
+
+            await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+        # self.tree.copy_global_to(guild=MY_GUILD)
+        # await self.tree.sync(guild=MY_GUILD)
+
+    async def close(self):
+        await super().close()
+        await self.session.close()
+
+    # @tasks.loop(minutes=10)
+    # async def background_task(self):
+    #     await self.change_presence(activity=discord.Activity(name=f">help ({len(self.guilds)} servers)", type=3))
+    #     print('Running background task...')
+    
+    @tasks.loop(minutes=30)
+    async def startup(self):
+        await self.wait_until_ready()
+        await self.change_presence(activity=discord.Activity(name=f">help on {len(self.guilds)} servers", type=3))
+
+    async def on_ready(self):
+        print('Bot is online!')
+
+
+async def main():
+    bot = Gizmo()
+
+    # , log_handler=handler, log_level=logging.DEBUG)
+    bot.run(DISCORD_BOT_TOKEN, )
+
+
+asyncio.run(main())
